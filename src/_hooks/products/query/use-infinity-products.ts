51bc986a88ee0ce331ec5fee557448a2a65e1dfd
kimhyojung <706shin1728@naver.com>
2025-01-28 12:01:07 +0900

    feat: 상품 리스트 무한스크롤을 위한 훅 구현

---
+++ src/_hooks/products/query/use-infinity-products.ts
@@ -0,0 +1,27 @@
+import { useSuspenseInfiniteQuery } from "@tanstack/react-query";
+import { getProducts } from "../../../_actions";
+
+interface UseGetInfinityProductsProp {
+  limit: number;
+  skip: number;
+}
+
+export function useGetInfinityProducts({
+  limit = 15,
+  skip = 0,
+}: UseGetInfinityProductsProp) {
+  return useSuspenseInfiniteQuery({
+    queryKey: [`/products/${limit}/${skip}`],
+    queryFn: () =>
+      getProducts({
+        skip,
+        limit,
+      }),
+    initialPageParam: 0,
+    getNextPageParam: (lastPage) => {
+      return lastPage.skip + lastPage.limit < lastPage.total
+        ? lastPage.skip + lastPage.limit
+        : undefined;
+    },
+  });
+}
